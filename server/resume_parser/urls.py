b145eba8714686a9bde1a723792f1885e63551fa
CodeSteaks <mianmujtaba89@gmail.com>
2023-07-27 14:46:08 +0500

    updates

--- server/resume_parser/urls.py
+++ server/resume_parser/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import match_resumes
 urlpatterns = [
@@ -25,4 +27,4 @@ urlpatterns = [
         match_resumes,
         name="match-reumes"
     )
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
